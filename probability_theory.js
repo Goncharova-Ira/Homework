dbc54b028a544efa0f4596b532d601b06da75455
Goncharova-Ira <goncharovaira2905@gmail.com>
2025-05-22 20:29:25 +0300

    change_variables_name

--- probability_theory.js
+++ probability_theory.js
@@ -1,20 +1,20 @@
 function checkProbabilityTheory(count) {
-    let countEvenNumber = 0
-    let countOddNumber = 0
+    let evenCount = 0
+    let oddCount = 0
     for (let i = 1; i <= count; i++) {
         rnd = Math.round(100 + Math.random() * (1000 - 100));
         console.log('згенероване число = ' + rnd);
         if (rnd % 2 === 0) {
-            countEvenNumber += 1
+            evenCount += 1
         } else {
-            countOddNumber += 1
+            oddCount += 1
         }
     }
-    let percentageEvenNumber = countEvenNumber/count *100;
-    let percentageOddNumber = countOddNumber/count *100;
+    let percentageEvenNumber = evenCount/count *100;
+    let percentageOddNumber = oddCount/count *100;
     console.log('кількість згенерованих чисел = ' + count)
-    console.log('кількість парних чисел = ' + countEvenNumber);
-    console.log('кількість непарних чисел = ' + countOddNumber);
+    console.log('кількість парних чисел = ' + evenCount);
+    console.log('кількість непарних чисел = ' + oddCount);
     console.log('відсоткове співвідношення парних до непарних = ' + percentageEvenNumber +'% / '+percentageOddNumber +'%')
 }
 checkProbabilityTheory(10)
