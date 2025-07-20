3fb4e0e5ae9e1ea48bd01e6f2bedd419c694d51c
Goncharova-Ira <goncharovaira2905@gmail.com>
2025-07-20 19:57:41 +0300

    Jmeter_Reports

--- without_a.js
+++
@@ -1,5 +0,0 @@
-const words = ["Wonderful", "Joyful", "Happiness", "Time", "Task", "Apple"];
-const regex = /^[^Aa]{6,}$/;
-
-const without_Aa = words.filter(word => regex.test(word));
-console.log(without_Aa);
